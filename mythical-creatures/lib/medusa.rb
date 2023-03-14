ca022554e61358dce4765c03381decd7368ab617
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-14 14:56:27 -0400

    Add methods and refactor for successful testing

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -1,39 +1,90 @@
+#refactored code
 class Medusa
-    attr_reader :name, :statues 
-    
+    attr_reader :name, :statues
     def initialize(name)
         @name = name
         @statues = []
     end
-
+    
     def stare(victim)
-        if statues.length <= 2
-        @statues << victim 
-       victim.stoned = true
-        else
-            old_victim = @statues.shift
-            old_victim.stoned = false
-            victim.stoned = true
-            @statues << victim
-        end
-      
+        @statues << victim
+        victim.make_stoned
+        process_extra_victims
     end
+    
+    def process_extra_victims
+        return false if statues.count <= 3
+        old_victim = @statues.shift
+        old_victim.make_unstoned
+    end
+
 
 end
 
 class Person
-    attr_reader :name 
-    attr_accessor :stoned
+    attr_reader :name, :stoned
 
     def initialize(name)
         @name = name
         @stoned = false
     end
 
+    def make_stoned
+        @stoned = true
+    end
+
+    def make_unstoned
+        @stoned = false
+    end
+
     def stoned?
-       stoned
+        stoned
     end
 
 end
 
+
+
+
+
+
+
 # require "pry"; binding.pry
+
+
+
+#initial code before above refactoring
+
+# class Medusa
+#      attr_reader :name, :statues 
+#      def initialize(name)
+#         @name = name
+#         @statues = []
+#     end
+
+    # def stare(victim)
+        # if statues.length <= 2
+        #   @statues << victim 
+        #   victim.stoned = true
+        #  else
+            # old_victim = @statues.shift
+            # old_victim.stoned = false
+            # @statues << victim
+        # end
+    # end
+# end
+
+# class Person
+#     attr_reader :name
+#     attr_accessor :stoned
+
+#     def initialize(name)
+#         @name = name
+#         @stoned = false
+#     end
+
+#     def stoned?
+#        stoned
+#     end
+
+# end
