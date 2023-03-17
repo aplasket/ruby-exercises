2af6a2185884ab507038308bb07308c8d0ec9399
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-17 15:52:45 -0400

    Erase and redo Direwolf for practice

--- mythical-creatures/lib/direwolf.rb
+++ mythical-creatures/lib/direwolf.rb
@@ -1,26 +1,25 @@
 class Direwolf
     attr_reader :name, :home, :size, :starks_to_protect, :hunts_white_walkers
-
     def initialize(name, home = "Beyond the Wall", size = "Massive")
         @name = name
         @home = home
-        @size = size
+        @size = size    
         @starks_to_protect = []
         @hunts_white_walkers = true
     end
 
     def protects(stark)
-        if stark.location == @home
-            @starks_to_protect << stark 
-            stark.is_protected
-            @hunts_white_walkers = false
-        end
-        process_extra_starks
+       if stark.location == @home
+         @starks_to_protect << stark
+         stark.is_protected
+         @hunts_white_walkers = false
+         process_extra_starks
+       end
     end
 
     def process_extra_starks
-        return false if starks_to_protect.count <= 2
-        extra_starks = @starks_to_protect.pop
+       return false if starks_to_protect.count <= 2
+       @starks_to_protect.pop 
     end
 
     def hunts_white_walkers?
@@ -33,6 +32,7 @@ class Direwolf
         stark
     end
 end
+
 
 class Stark
     attr_reader :name, :location, :safe, :house_words
@@ -54,4 +54,5 @@ class Stark
     def is_not_protected
         @safe = false
     end
+
 end
